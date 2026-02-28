c3abb4f315f2689ec653079aa312080f0e8651e5
Cong Luan Tran <congluan.tran19@gmail.com>
2026-02-28 17:06:28 +0700

    hotfix: add dependencies build to pom, removed problematic classes

--- backend/src/main/java/com/kuizu/backend/repository/ClassStatisticRepository.java
+++
@@ -1,9 +0,0 @@
-package com.kuizu.backend.repository;
-
-import com.kuizu.backend.entity.ClassStatistic;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ClassStatisticRepository extends JpaRepository<ClassStatistic, Long> {
-}
